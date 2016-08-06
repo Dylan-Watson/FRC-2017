93c5d2c7eaa84c1bbcbb4e6cd745e72c8644d725
Dylan-Watson <dylantrwatson@gmail.com>
2016-08-05 22:18:18 -0500

    Dio

--- Base/Components/DioItem.cs
+++ Base/Components/DioItem.cs
@@ -4,29 +4,34 @@ using WPILib;
 namespace Base.Components
 {
     /// <summary>
+    ///     Defines the type of DIO
     /// </summary>
     public enum DioType
     {
         /// <summary>
+        ///     Digital Input
         /// </summary>
         Input,
 
         /// <summary>
+        ///     Digital Output
         /// </summary>
         Output
     }
 
     /// <summary>
+    ///     Class to handle Digital Input and Digital Output Components
     /// </summary>
     public class DioItem : IO, IComponent
     {
         private readonly DigitalSource dio;
 
         /// <summary>
+        ///     Constructor
         /// </summary>
-        /// <param name="type"></param>
-        /// <param name="channel"></param>
-        /// <param name="commonName"></param>
+        /// <param name="type">type of DIO</param>
+        /// <param name="channel">pwm channel the DIO is plugged into</param>
+        /// <param name="commonName">CommonName the component will have</param>
         public DioItem(DioType type, int channel, string commonName)
         {
             if (type == DioType.Input)
@@ -38,37 +43,43 @@ namespace Base.Components
         }
 
         /// <summary>
+        ///     Defines whether the component is in use or not
         /// </summary>
         public bool InUse { get; private set; }
 
         /// <summary>
+        ///     Name of the component
         /// </summary>
         public string Name { get; }
 
         /// <summary>
+        ///     Defines the object issuing the commands
         /// </summary>
         public object Sender { get; private set; }
 
         /// <summary>
+        ///     returns dio
         /// </summary>
-        /// <returns></returns>
+        /// <returns>dio</returns>
         public object GetRawComponent()
         {
             return dio;
         }
 
         /// <summary>
+        ///     Gets the Input Value from the DigitalInput
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Double</returns>
         public bool Get()
         {
             return ((DigitalInput) dio).Get();
         }
 
         /// <summary>
+        ///     Sets a value to the DigitalOutput
         /// </summary>
-        /// <param name="val"></param>
-        /// <param name="sender"></param>
+        /// <param name="val">value to set the controller to</param>
+        /// <param name="sender">the caller of this method</param>
         protected override void set(double val, object sender)
         {
             Sender = sender;
