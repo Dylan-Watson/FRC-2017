d3a56a7f58a2b4e62d175d48125f12067f858843
Dylan R Watson <dylantrwatson@gmail.com>
2017-01-16 17:15:56 -0600

    Completely deprecated master/slave Victor

--- RobotMain2017/RobotMain2017.cs
+++ RobotMain2017/RobotMain2017.cs
@@ -59,10 +59,9 @@ namespace RobotMain2017
 
         #region Private Fields
 
-        private const string CONFIG_FILE = @"robot.xml";
-        //private const string CONFIG_FILE = @"MasterTest.xml";
+        //private const string CONFIG_FILE = @"robot.xml";
         //private const string CONFIG_FILE = @"robot_test_kit.xml";
-        //private const string CONFIG_FILE = @"eltoro.xml";
+        private const string CONFIG_FILE = @"eltoro.xml";
 
         private readonly Config config = new Config();
 
