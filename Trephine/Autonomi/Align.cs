f8bebf59131668871a66b0dd11fc879d93d5ea54
Ryan S. Cooper <Ryan S. Cooper@BRONCBOTZ-NT>
2017-01-20 20:31:41 -0600

    cool stuff

--- Trephine/Autonomi/Align.cs
+++ Trephine/Autonomi/Align.cs
@@ -8,6 +8,9 @@ namespace Trephine.Autonomi
         const double GAMMA = .011;
         const double ALPHA = .20;
 
+        const double GAMMA2 = .075;
+        const double ALPHA2 = .20;
+
         protected override void main()
         {
             var target = VisionMonitor.Instance.GetLatestTargetData(0).Target;
@@ -23,32 +26,32 @@ namespace Trephine.Autonomi
             if (offset > 20)
             {
                 baseCalls.SetLeftDrive(-getSpeed(offset, GAMMA, ALPHA));
-                baseCalls.SetRightDrive(getSpeed(offset, GAMMA, ALPHA));
+                baseCalls.SetRightDrive(-getSpeed(offset, GAMMA, ALPHA));
             }
 
             else if (offset < -20)
             {
                 baseCalls.SetLeftDrive(-getSpeed(offset, GAMMA, ALPHA));
-                baseCalls.SetRightDrive(getSpeed(offset, GAMMA, ALPHA));
+                baseCalls.SetRightDrive(-getSpeed(offset, GAMMA, ALPHA));
             }
-            else if (offset < 20 && getOffset(target) > -20)
+            else if (offset < 20 && offset > -20)
             {
                 /*get the diameter of the target at current position 
                  * assume diameter should be 15 units
                  */
-                var diameter = target.Radius * 2;
+                var diameter = getDistanceOffset(target);
                 /*check if the diameter is in the "o.k." range (assumed +/- 5 units), if not, adjust
                  * stop once botty bot gets in the "o.k." range
                  */
-                if (diameter > 20)
+                if (diameter > 0)
                 {
-                    baseCalls.SetLeftDrive(-getSpeed(offset, GAMMA, ALPHA));
-                    baseCalls.SetRightDrive(-getSpeed(offset, GAMMA, ALPHA));
+                    baseCalls.SetLeftDrive(-getSpeed(diameter, GAMMA2, ALPHA2));
+                    baseCalls.SetRightDrive(getSpeed(diameter, GAMMA2, ALPHA2));
                 }
-                else if (diameter < 10)
+                else if (diameter < 0)
                 {
-                    baseCalls.SetLeftDrive(getSpeed(offset, GAMMA, ALPHA));
-                    baseCalls.SetRightDrive(getSpeed(offset, GAMMA, ALPHA));
+                    baseCalls.SetLeftDrive(-getSpeed(diameter, GAMMA2, ALPHA2));
+                    baseCalls.SetRightDrive(getSpeed(diameter, GAMMA2, ALPHA2));
                 }
                 else
                     baseCalls.FullStop();
@@ -68,6 +71,18 @@ namespace Trephine.Autonomi
                 return target.X - centre;
             else if (target.X < centre)
                 return -(centre - target.X);
+
+            return 0;
+        }
+
+        private int getDistanceOffset(Target target)
+        {
+            var centre = 60;
+
+            if (target.Radius > centre)
+                return target.Radius - centre;
+            else if (target.Radius < centre)
+                return -(centre - target.Radius);
 
             return 0;
         }
